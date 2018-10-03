5eb7343d8a6cb72505bbdfb58778dbfdba4c1843
Fernando Brandao <fwbrandao@gmail.com>
2018-10-03 08:56:41 +0100

    small changes to profile

---
+++ src/app/profile.component.ts
@@ -0,0 +1,32 @@
+import { Component } from "@angular/core";
+import { ApiService } from "./api.service";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "profile",
+  template: `
+    <mat-card>
+    <mat-card-header>
+    <mat-card-title>
+    <h4>Profile</h4>
+    </mat-card-title>
+    </mat-card-header>
+    <mat-card-content>
+        <mat-list-item>Name: {{profile?.name}}</mat-list-item>
+        <mat-list-item>Email: {{profile?.email}}</mat-list-item>
+        <mat-list-item>Description: {{profile?.description}}</mat-list-item>
+    </mat-card-content>
+    </mat-card>
+    `
+})
+export class ProfileComponent {
+  constructor(private apiService: ApiService, private route: ActivatedRoute) {}
+
+  profile;
+
+  ngOnInit() {
+    const id = this.route.snapshot.params.id;
+    // console.log(id);
+    this.apiService.getProfile(id).subscribe(data => this.profile = data.json());
+  }
+}
