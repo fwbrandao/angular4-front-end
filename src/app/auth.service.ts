dff57c6c7f998dbfa09764d983523d447acd93e1
Fernando Brandao <fwbrandao@gmail.com>
2018-10-03 08:24:04 +0100

    added to auth.service

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,21 +1,24 @@
-import { Http } from "@angular/http";
-import { Injectable } from "@angular/core";
+import { Http } from '@angular/http';
+import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthService {
+  messages = [];
+  path = 'http://localhost:3000/auth';
+
 
   constructor(private http: Http) {}
 
   sendUserRegistration(registerData) {
     this.http
-      .post("http://localhost:3000/register", registerData)
+      .post(this.path + '/register', registerData)
       .subscribe(res => {});
   }
 
   loginUser(loginData) {
-    this.http.post("http://localhost:3000/login", loginData).subscribe(res => {
+    this.http.post(this.path + '/login', loginData).subscribe(res => {
       console.log(res);
-      localStorage.setItem("token", res.json().token);
+      localStorage.setItem('token', res.json().token);
     });
   }
 }
